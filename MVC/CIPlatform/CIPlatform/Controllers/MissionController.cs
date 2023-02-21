378707c881bd9c3de26f6623dff0cc116457ab3a
Jenil Vora <jenil.vora0408@gmail.com>
2023-02-21 18:16:17 +0530

    Completed Grid View, List View & No Mission Found of Home - Mission Listing Page

---
+++ MVC/CIPlatform/CIPlatform/Controllers/MissionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CIPlatform.Controllers
+{
+    public class MissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MissionListing()
+        {
+            return View();
+        }
+
+        public IActionResult MissionListing_List()
+        {
+            return View();
+        }
+    }
+}
