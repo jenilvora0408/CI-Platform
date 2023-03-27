6585d1c72355ddb73c02ea8ebd242048f781e6f8
Jenil Vora <jenil.vora0408@gmail.com>
2023-03-27 18:29:49 +0530

    Made AJAX Call for saving data in story table

---
+++ HTML/js/hello.js
@@ -0,0 +1,167 @@
+
+function ImgUpload() {
+    var imgWrap = "";
+    var imgArray = [];
+
+    $('.upload__inputfile').each(function() {
+    $(this).on('change', function(e) {
+        imgWrap = $(this).closest('.upload__box').find('.upload__img-wrap');
+        var maxLength = $(this).attr('data-max_length');
+        var files = e.target.files;
+        var filesArr = Array.prototype.slice.call(files);
+        var iterator = 0;
+        var formData = new FormData();
+
+
+        filesArr.forEach(function(f, index) {
+            // Check if file is an image with the correct extension
+            if (!f.type.match('image.(png|jpe?g)')) {
+                alert('Please select only .png, .jpg, or .jpeg images.');
+                return;
+            }
+
+            // Check if file size is less than 4 MB
+            if (f.size > 4 * 1024 * 1024) {
+                alert('Please select an image that is less than 4 MB.');
+                return;
+            }
+
+            formData.append("files", f);
+
+            if (imgArray.length > maxLength) {
+                return false;
+            } else {
+                var len = 0;
+                for (var i = 0; i < imgArray.length; i++) {
+                    if (imgArray[i] !== undefined) {
+                        len++;
+                    }
+                }
+                if (len > maxLength) {
+                    return false;
+                } else {
+                    imgArray.push(f);
+
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        var html = "<div class='upload__img-box'><div style='background-image: url(" + e.target.result + ")' data-number='" + $(".upload__img-close").length + "' data-file='" + f.name + "' class='img-bg'><div class='upload__img-close'></div></div></div>";
+                        imgWrap.append(html);
+                        iterator++;
+                    }
+                    reader.readAsDataURL(f);
+                }
+            }
+        });
+
+        // Send image data to server via AJAX
+        $("#saveBtn").on("click", function(e){
+              $.ajax({
+            url: "/Story/UploadImages",
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+            }
+        })
+      
+        });
+    });
+});
+
+
+    $('body').on('click', ".upload__img-close", function(e) {
+        var file = $(this).parent().data("file");
+        for (var i = 0; i < imgArray.length; i++) {
+            if (imgArray[i].name === file) {
+                imgArray.splice(i, 1);
+                break;
+            }
+        }
+        $(this).parent().parent().remove();});
+    };
+
+
+    $('.upload__box').on('dragover', function(e) {
+        e.preventDefault();
+        $(this).addClass('dragover');
+    });
+
+    $('.upload__box').on('dragleave', function(e) {
+        e.preventDefault();
+        $(this).removeClass('dragover');
+    });
+
+    $('.upload__box').on('drop', function(e) {
+        e.preventDefault();
+        $(this).removeClass('dragover');
+        imgWrap = $(this).closest('.upload__box').find('.upload__img-wrap');
+        var maxLength = $(this).find('.upload__inputfile').attr('data-max_length');
+
+        var files = e.originalEvent.dataTransfer.files;
+        var filesArr = Array.prototype.slice.call(files);
+        var iterator = 0;
+
+
+       filesArr.forEach(function(f, index) {
+            // Check if file is an image with the correct extension
+            if (!f.type.match('image.(png|jpe?g)')) {
+                alert('Please select only .png, .jpg, or .jpeg images.');
+                return;
+            }
+
+            // Check if file size is less than 4 MB
+            if (f.size > 4 * 1024 * 1024) {
+                alert('Please select an image that is less than 4 MB.');
+                return;
+            }
+
+            formData.append("files", f);
+
+            if (imgArray.length > maxLength) {
+                return false;
+            } else {
+                var len = 0;
+                for (var i = 0; i < imgArray.length; i++) {
+                    if (imgArray[i] !== undefined) {
+                        len++;
+                    }
+                }
+                if (len > maxLength) {
+                    return false;
+                } else {
+                    imgArray.push(f);
+
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        var html = "<div class='upload__img-box'><div style='background-image: url(" + e.target.result + ")' data-number='" + $(".upload__img-close").length + "' data-file='" + f.name + "' class='img-bg'><div class='upload__img-close'></div></div></div>";
+                        imgWrap.append(html);
+                        iterator++;
+                    }
+                    reader.readAsDataURL(f);
+                }
+            }
+        });
+        // Send image data to server via AJAX
+        $("#saveBtn").on("click", function(e){
+              $.ajax({
+            url: "/Story/UploadImages",
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+             }
+        })
+      
+        
+    });
+});
--- MVC/CIPlatform/CIPlatform/Controllers/StoryController.cs
+++ MVC/CIPlatform/CIPlatform/Controllers/StoryController.cs
@@ -20,13 +20,15 @@ namespace CIPlatform.Controllers
         private readonly ILogger<AccountController> _logger;
         private readonly MissionInterface _missionInterface;
         private readonly CiPlatformContext _ciPlatformContext;
+        private readonly IWebHostEnvironment _env;
 
-        public StoryController(ILogger<AccountController> logger, MissionInterface missionInterface, CiPlatformContext ciPlatformContext)
+        public StoryController(ILogger<AccountController> logger, MissionInterface missionInterface, CiPlatformContext ciPlatformContext, IWebHostEnvironment env)
         {
             _logger = logger;
             _missionInterface = missionInterface;
             _ciPlatformContext = ciPlatformContext;
-           
+            _env = env;
+
         }
 
         public IActionResult Index()
@@ -80,13 +82,53 @@ namespace CIPlatform.Controllers
         }
       
         
-         public IActionResult userAplliedMission()
-         {
+        public IActionResult userAplliedMission()
+        {
+           string userSessionEmailId = HttpContext.Session.GetString("useremail");
+           User userObj = _missionInterface.findUser(userSessionEmailId);
+           IEnumerable<MissionApplication> missionApplications = _ciPlatformContext.MissionApplications.Where(x => x.UserId == userObj.UserId && x.ApprovalStatus == "Approved").Include(x => x.Mission).AsEnumerable();
+            return Json(new { data = missionApplications });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImages(List<IFormFile> files, long StoryId)
+        {
+            foreach (var file in files)
+            {
+                // Save the file to the desired folder
+                var fileName = Path.GetFileName(file.FileName);
+                var filePath = Path.Combine(_env.WebRootPath, "uploads", fileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                StoryMedium storymedia = new StoryMedium();
+                storymedia.Path = "/uploads/" + fileName;
+                storymedia.Type = file.ContentType.Substring(6,9);
+                
+                storymedia.StoryId = StoryId;
+
+                _ciPlatformContext.StoryMedia.Add(storymedia);
+                _ciPlatformContext.SaveChangesAsync();
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult storyInsert(long DropdownItem,string TitleOfStory,string StoryDate,string EditorText)
+        {
             string userSessionEmailId = HttpContext.Session.GetString("useremail");
             User userObj = _missionInterface.findUser(userSessionEmailId);
-            IEnumerable<MissionApplication> missionApplications = _ciPlatformContext.MissionApplications.Where(x => x.UserId == userObj.UserId && x.ApprovalStatus == "Approved").Include(x => x.Mission).AsEnumerable();
-             return Json(new { data = missionApplications });
-         }
-        
+            Story story = new Story();
+            story.Title = TitleOfStory;
+            story.MissionId = DropdownItem;
+            story.CreatedAt  = DateTime.Now;
+            story.Description = EditorText;
+            story.UserId = userObj.UserId;
+            _ciPlatformContext.Stories.Add(story);
+            _ciPlatformContext.SaveChanges();
+            return Ok(story.StoryId);
+        }
     }
 }
